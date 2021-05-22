42aa406beed26319e3430573b88b9d7d8022c1bb
ymegane <crybaby.sumire+github@gmail.com>
2021-05-22 16:33:56 +0900

    fetch gmail messages

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,16 @@ const appEnvironments: Environments = {
     lineToken: properties.getProperty('LINE_TOKEN'),
 };
 
+const intervalMinute = 60; // 1時間
+
+const fetchMessages = (): GoogleAppsScript.Gmail.GmailMessage[][] => {
+    const currentTime = Math.floor(new Date().getTime() / 1000);
+    const timeTerm = currentTime - 60 * intervalMinute;
+    const query = `is:unread after:${timeTerm}`;
+
+    const threads = GmailApp.search(query);
+    const messages = GmailApp.getMessagesForThreads(threads);
+    return messages;
+};
+
 function main() {}
